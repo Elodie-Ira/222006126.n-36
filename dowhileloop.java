2c026315f7cbceca4968f36ea6e141b6eb6f5d47
Elodie-Ira <149666643+Elodie-Ira@users.noreply.github.com>
2023-11-04 16:58:36 +0200

    Update dowhileloop.java

--- dowhileloop.java
+++ dowhileloop.java
@@ -1,10 +1,11 @@
+//The program that displays numbers from 1  up to 5 using do...while loop
 public class dowhileloop {
     public static void main(String[] args){
-        int i = 1;
+        int i = 1;// The initial variable 
         do{
-            System.out.println("18");
+            System.out.println("18");//The output
             i++;
         }
-        while(i<=5);
+        while(i<=5);//The final variable
     }
 }
