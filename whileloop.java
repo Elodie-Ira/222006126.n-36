369754bc810165e093d40d2ec0458f267d5ad817
Elodie-Ira <149666643+Elodie-Ira@users.noreply.github.com>
2023-11-04 17:49:53 +0200

    Update whileloop.java

--- whileloop.java
+++ whileloop.java
@@ -1,9 +1,10 @@
-public class whileloop {
+//The program that asks the user to enter the numbers using while loop
+public class whileloop {//class name
     public static void main(String[] args){
-        int i = 1;
-        while(i<=10){
-            System.out.println("*");
-            i++;
+        int i = 1;//initial declaration
+        while(i<=10){//final declaration
+            System.out.println("*");//the output
+            i++;//incrementation
         }
     }
 }
