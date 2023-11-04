063bd2b1ae381d1f16efeb166b0d72fbf3a8b244
Elodie-Ira <149666643+Elodie-Ira@users.noreply.github.com>
2023-11-04 17:15:10 +0200

    Update nestedforloop.java

--- nestedforloop.java
+++ nestedforloop.java
@@ -1,10 +1,11 @@
-public class nestedforloop {
+//The program that asks the user to display the numbers from 1 up to 5 using the nested for loop
+public class nestedforloop {//class name
      public static void main(String[] args) {
-        for(int i = 1; i <= 5; i++){
-            for(int j=1; j<=i; j++){
-                System.out.print (i);
+        for(int i = 1; i <= 5; i++){//initial and final initialization for the rows 
+            for(int j=1; j<=i; j++){//initial and final initialization for the columns
+                System.out.print (i);//the ouput
             } 
-            System.out.println();           
+            System.out.println(); //The space that comes between the numbers          
         }
      }
 }
