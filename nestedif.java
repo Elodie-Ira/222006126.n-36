d817f336bf2ffc0b56b84f491e96087f368b6605
Elodie-Ira <149666643+Elodie-Ira@users.noreply.github.com>
2023-11-04 17:24:40 +0200

    Update nestedif.java

--- nestedif.java
+++ nestedif.java
@@ -1,17 +1,18 @@
-public class nestedif {
+//The program that asks the user to enter marks of student using nestedif
+public class nestedif {//class name
     public static void main(String[] args){
-        int marks = 4;
-         if (marks>15){
-            System.out.println("a");
+        int marks = 4;//initialisation
+         if (marks>15){//condition
+            System.out.println("a");//the output
          } 
-         else if (marks>10){
-            System.out.println("b");
+         else if (marks>10){//condition
+            System.out.println("b");//the output
          } 
-         else if (marks>5){
-            System.out.println("c");
+         else if (marks>5){//condition
+            System.out.println("c");//the output
          } 
          else{
-            System.out.println("F");
+            // System.out.println("F");//the output
          }
     }
 }
