144b9d377cb15dcd1a0a460a5290f87aa794721c
Elodie-Ira <149666643+Elodie-Ira@users.noreply.github.com>
2023-11-04 16:05:21 +0200

    Update App.java

--- App.java
+++ App.java
@@ -1,5 +1,6 @@
+//This is the java program that asks the user to enter the word "Hello world"
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");// This is the output
     }
 }
