6844ab22ab29ce03e7ff04c082ba554374d76476
Elodie-Ira <149666643+Elodie-Ira@users.noreply.github.com>
2023-11-04 17:31:56 +0200

    Update simpleif.java

--- simpleif.java
+++ simpleif.java
@@ -1,10 +1,11 @@
-public class simpleif {
+//The program that asks the user to enter the 80
+public class simpleif {//class name
     public static void main(String[] args) {
-        int number = 80;
-        if (number == 80) {
-            System.out.println("true");
+        int number = 80;//Initiliastion
+        if (number == 80) {//The  condition
+            System.out.println("true");//the output
         } else {
-            System.out.println("false");
+            System.out.println("false");//the output
         }
 
     }
