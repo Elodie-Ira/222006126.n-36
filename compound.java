aa63501f68410ece873cdcbbc1afe683eeebce73
Elodie-Ira <149666643+Elodie-Ira@users.noreply.github.com>
2023-11-04 16:53:04 +0200

    Update compound.java

--- compound.java
+++ compound.java
@@ -1,12 +1,13 @@
+//The program that asks the user to enter the compound interest annually
 public class compound{
     public static int interest(int p,int r,int t){
-        int result=p*(1+r)^t;
+        int result=p*(1+r)^t;//The formula of the compound interest
         return result;
 
     }
     public static void main( String[] args){
         compound q1= new compound();
-        double interest= q1.interest(50000,18,3);
-        System.out.println("The compound interest of Mr Bob is:" +" " +interest);
+        double interest= q1.interest(50000,18,3);//Calling the object name
+        System.out.println("The compound interest of Mr Bob is:" +" " +interest);//The output
     }
 }
