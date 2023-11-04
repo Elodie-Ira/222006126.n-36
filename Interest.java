8a918ab89e306b32a49057c031abf8f28269ce3f
Elodie-Ira <149666643+Elodie-Ira@users.noreply.github.com>
2023-11-04 16:27:01 +0200

    Update Interest.java

--- Interest.java
+++ Interest.java
@@ -1,12 +1,13 @@
-public class Interest {
+//This program asks the user to calculate  the compound interest using objects and methods  
+public class Interest {//class name
         public static double compound(double p, double r, double t, double n){
-            double result=p*Math.pow(1+(r/n),n*t);
+            double result=p*Math.pow(1+(r/n),n*t);//Formula of calculating the compound interest
             return result;
         }
         public static void main(String[] args){
             Interest f1=new Interest();
-            double compound=f1.compound(500000,0.18,3,12);
-            System.out.println("The compound interest of Mr Bob is"+" " +compound); 
+            double compound=f1.compound(500000,0.18,3,12);//Calling the object name
+            System.out.println("The compound interest of Mr Bob is"+" " +compound);//The output of compound interest 
         }
     }
 
