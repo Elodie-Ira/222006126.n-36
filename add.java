715bcd00082931c8a5f5188999ebe4bd9c0bc9f0
Elodie-Ira <149666643+Elodie-Ira@users.noreply.github.com>
2023-11-04 17:52:49 +0200

    Update add.java

--- add.java
+++ add.java
@@ -1,5 +1,5 @@
 //This program that asks the user to enter the modulus of the two numbers 
-public class add {
+public class add {//class name
     public static void main(String a[]){
         int fn = 30;//a variable of first number 
         int sn = 20;//a variable of second  number
