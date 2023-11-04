f5f76364683549b60dedebce044c87b7e4066160
Elodie-Ira <149666643+Elodie-Ira@users.noreply.github.com>
2023-11-04 16:14:16 +0200

    Update Elodie.java

--- Elodie.java
+++ Elodie.java
@@ -1,10 +1,12 @@
-class Elodie{
+//The java program that asks the user to enter the numbers from 1 up to 10 using the break statement
+public class Elodie{ // This is classname of the program
      public static void main(String[] args) {
-        for(int i = 0; i <= 10; i++){
+        for(int i = 0; i <= 10; i++){ 
             if(i==5){
                  break;
             } 
-            System.out.println(i);
+           //This is the output
+             System.out.println(i);
                
 
             }
