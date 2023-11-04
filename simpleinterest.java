6c8249282f17619d0b5568dfb3c41b140ee11868
Elodie-Ira <149666643+Elodie-Ira@users.noreply.github.com>
2023-11-04 17:37:10 +0200

    Update simpleinterest.java

--- simpleinterest.java
+++ simpleinterest.java
@@ -1,12 +1,13 @@
-public class simpleinterest{
+//The program that asks the user to calculate simple interest
+public class simpleinterest{//The class name
     public static double interest(double p,double r,int t){
-        double result=(p*r*t)/100;
+        double result=(p*r*t)/100;//Formula 
         return result;
 
     }
     public static void main(String [] args){
-        simpleinterest a2=new simpleinterest();
-        double interest = a2.interest(100000,5,5);
-        System.out.println("The Interest is:"+" "+ interest);
+        simpleinterest a2=new simpleinterest();//Calling the function name
+        double interest = a2.interest(100000,5,5);// Calling the object name
+        System.out.println("The Interest is:"+" "+ interest);//The output
     }
 }
