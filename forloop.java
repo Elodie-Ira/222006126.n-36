1877923d915d92ef7bd3b227aba8c4f78529e51d
Elodie-Ira <149666643+Elodie-Ira@users.noreply.github.com>
2023-11-04 17:54:54 +0200

    Update forloop.java

--- forloop.java
+++ forloop.java
@@ -1,5 +1,5 @@
 //The program that asks the user to enter the numbers from 1 up to 10
-public class forloop {
+public class forloop {// class name
      public static void main(String[] args) {
         for(int i = 1; i <= 10; i++){// the initial and final initialization
             
