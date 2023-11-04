e5958242e727ecd5b28a93f0ae54aac9b16dfc1e
Elodie-Ira <149666643+Elodie-Ira@users.noreply.github.com>
2023-11-04 17:43:26 +0200

    Update switchcase.java

--- switchcase.java
+++ switchcase.java
@@ -1,18 +1,19 @@
-public class switchcase {
+//The program that asks the user to enter days using switchcase
+public class switchcase {//The class name
     public static void main(String[] args){
-        int day = 4;
+        int day = 4;//Initialisation
         switch(day){
             case 1:
-              System.out.println("monday");
+              System.out.println("monday");//The output
               break;
             case 2:
-              System.out.println("tuesday");
+              System.out.println("tuesday");//The output
               break;
             case 3:
-              System.out.println("wednesday");
+              System.out.println("wednesday");//The output
               break;
             default:
-              System.out.println("unknown");
+              System.out.println("unknown");//The output
               break;      
 
         }
