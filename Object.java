d46aa6f9ac3145f0ace6d8afc5a7b0518b02a102
Elodie-Ira <149666643+Elodie-Ira@users.noreply.github.com>
2023-11-04 16:33:30 +0200

    Update Object.java

--- Object.java
+++ Object.java
@@ -1,12 +1,14 @@
+//The program that asks the user to sum up two numbers using object
 public class Object {
     public static int addNumbers(int Num1, int Num2){
         return Num1 + Num2;
     }
     public static void main(String args[]){
-        int number1 = 5;
-        int number2 = 7;
+        int number1 = 5;//a variable of  number1
+        int number2 = 7;//a variable of  number2
 
         int result = addNumbers(number1, number2);
+       //The output of the program
         System.out.println("The sum of " +" " +number1 + "and "+" "+number2+"is"+" "+result);
     }
 }
